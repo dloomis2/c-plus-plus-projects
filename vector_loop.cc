336ac286b70f4fb9785e38c273ee3a202e034eb8
David Loomis <dloomis2@csu.fullerton.edu>
2025-03-11 00:28:45 -0700

    working on this loop with vectors

---
+++ vector_loop.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char *argv[]){
+  std::vector<std::string> command_line {argv, argv + argc};
+  std::cout << command_line.at(0) << '\n';
+  
+  std::vector<int> counter_vect {1,2,3,4,5,6,7,8,9,10};
+  std::cout << counter_vect.size();
+
+	for
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
